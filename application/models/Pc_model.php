4d580a84844bd8615bd86802c09f8eef6a4b33b6
Rundredoffi <contact.rundred@gmail.com>
2024-11-15 09:24:38 +0100

    CORE : Ajout page de test

---
+++ application/models/Pc_model.php
@@ -0,0 +1,13 @@
+<?php
+class Pc_model extends CI_Model {
+/** 
+ * Classe d'accès aux données 
+ * Hérite de la classe CI_Model
+ */
+
+    public function __construct()
+    {
+        $this->load->database();
+    }
+    
+}
