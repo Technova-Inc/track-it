cd735096eb59b5b3bf342f2a45e47a2ff80082e6
Rundredoffi <contact.rundred@gmail.com>
2024-11-22 09:43:14 +0100

    FIX : Changement nom boutons

--- application/views/consultation/v_cons_software.php
+++ application/views/consultation/v_cons_software.php
@@ -1,10 +1,9 @@
 <div class="d-flex vh-100">
         <div class="container content-center">
-            <h1 class="mb-3 text-center">Software</h1>
             <div class="row content-center">
                 <div class="card h-100">
                     <div class="card-header">
-                        <h5 class="mb-0">Software</h5>
+                        <h5 class="mb-0">Applications</h5>
                     </div>
                     <div class="card-body">
                         <div class="table-responsive">
@@ -24,54 +23,6 @@
                                         <td class="text-start">21H2</td>
                                         <td class="text-start">Stable version</td>
                                     </tr>
-                                    <tr>
-                                        <td class="text-start">Linux Foundation</td>
-                                        <td class="text-start">Ubuntu</td>
-                                        <td class="text-start">22.04</td>
-                                        <td class="text-start">LTS release</td>
-                                    </tr>
-                                    <tr>
-                                        <td class="text-start">Apple</td>
-                                        <td class="text-start">macOS Monterey</td>
-                                        <td class="text-start">12.6</td>
-                                        <td class="text-start">For Apple Silicon</td>
-                                    </tr>
-                                    <tr>
-                                        <td class="text-start">Mozilla</td>
-                                        <td class="text-start">Firefox</td>
-                                        <td class="text-start">116.0</td>
-                                        <td class="text-start">Web browser with privacy features</td>
-                                    </tr>
-                                    <tr>
-                                        <td class="text-start">JetBrains</td>
-                                        <td class="text-start">IntelliJ IDEA</td>
-                                        <td class="text-start">2024.1</td>
-                                        <td class="text-start">IDE for Java development</td>
-                                    </tr>
-                                    <tr>
-                                        <td class="text-start">Adobe</td>
-                                        <td class="text-start">Photoshop</td>
-                                        <td class="text-start">2024.0</td>
-                                        <td class="text-start">Image editing software</td>
-                                    </tr>
-                                    <tr>
-                                        <td class="text-start">Microsoft</td>
-                                        <td class="text-start">Visual Studio Code</td>
-                                        <td class="text-start">1.79</td>
-                                        <td class="text-start">Lightweight code editor</td>
-                                    </tr>
-                                    <tr>
-                                        <td class="text-start">Autodesk</td>
-                                        <td class="text-start">AutoCAD</td>
-                                        <td class="text-start">2024</td>
-                                        <td class="text-start">Software for 2D and 3D CAD design</td>
-                                    </tr>
-                                    <tr>
-                                        <td class="text-start">Valve</td>
-                                        <td class="text-start">Steam</td>
-                                        <td class="text-start">2024</td>
-                                        <td class="text-start">Gaming platform and store</td>
-                                    </tr>
                                 </tbody>
                             </table>
                         </div>
@@ -80,7 +31,7 @@
                         <nav aria-label="Page navigation example" class="pagination-container">
                             <ul class="pagination justify-content-end pagination-sm">
                                 <li class="page-item">
-                                    <a class="page-link" href="#">Previous</a>
+                                    <a class="page-link" href="#">Précédent</a>
                                 </li>
                                 <li class="page-item">
                                     <a class="page-link" href="#">1</a>
@@ -92,7 +43,7 @@
                                     <a class="page-link" href="#">3</a>
                                 </li>
                                 <li class="page-item">
-                                    <a class="page-link" href="#">Next</a>
+                                    <a class="page-link" href="#">Suivant</a>
                                 </li>
                             </ul>
                         </nav>
@@ -144,7 +95,7 @@
                         <nav aria-label="Page navigation example" class="pagination-container">
                             <ul class="pagination justify-content-end pagination-sm">
                                 <li class="page-item">
-                                    <a class="page-link" href="#">Previous</a>
+                                    <a class="page-link" href="#">Précédent</a>
                                 </li>
                                 <li class="page-item">
                                     <a class="page-link" href="#">1</a>
@@ -156,7 +107,7 @@
                                     <a class="page-link" href="#">3</a>
                                 </li>
                                 <li class="page-item">
-                                    <a class="page-link" href="#">Next</a>
+                                    <a class="page-link" href="#">Suivant</a>
                                 </li>
                             </ul>
                         </nav>
