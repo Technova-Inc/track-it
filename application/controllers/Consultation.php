08026e76e51775440fea1562d9a675577d7ec226
Rundredoffi <contact.rundred@gmail.com>
2024-11-29 08:54:04 +0100

    CORE : Ajout de la traduction Française

--- application/controllers/Consultation.php
+++ application/controllers/Consultation.php
@@ -1,5 +1,6 @@
 <?php
 class Consultation extends CI_Controller {
+    private $nom;
     public function __construct(){
         parent::__construct();
         $this->load->model('Pc_model');
@@ -8,15 +9,28 @@ class Consultation extends CI_Controller {
     }
     public function index()
     {
-        $this->commun();
+        $nomPc = $this->input->post_get('nomPc', TRUE);
+        if($nomPc == NULL){
+            $this->noPc();
+        }else{
+            $this->commun($nomPc);
+        }
     }
-    private function commun(){
-        $nomPc = "322-ACR-M4630G";
-        $data['pc'] = $this->Pc_model->get_Infos_cons_main($nomPc);
+    private function commun($nomPc){
         $this->load->view('structure/v_page_entete');
         $this->load->view('structure/v_page_menu');
+        $data['pc'] = $this->Pc_model->get_Infos_cons_main($nomPc);
         $this->load->view('consultation/v_cons_main',$data);
         $this->load->view('consultation/v_cons_software');
+        $data['network'] = $this->Pc_model->get_Infos_cons_network($nomPc);
+
+        $this->load->view('consultation/v_cons_network');
+        $this->load->view('structure/v_page_pied');
+    }
+    private function noPc(){
+        $this->load->view('structure/v_page_entete');
+        $this->load->view('structure/v_page_menu');
+        $this->load->view('consultation/v_cons_noPc');
         $this->load->view('structure/v_page_pied');
     }
 }
---
+++ application/views/consultation/v_cons_noPc.php
@@ -0,0 +1,1 @@
+Aucun pc trouvé
