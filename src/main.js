521dff0fc068f85cbe3813073e6bdf4cdb02cf9d
Rundredoffi <contact.rundred@gmail.com>
2025-02-28 07:01:16 +0100

    ✨ Ajout de Vuetify comme dépendance et configuration du plugin

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
+import router from '@/router'
+import vuetify from './plugins/vuetify'
 import App from './App.vue'
-import router from './router'
 
 import CoreuiVue from '@coreui/vue'
 import CIcon from '@coreui/icons-vue'
@@ -13,6 +13,7 @@ const app = createApp(App)
 app.use(createPinia())
 app.use(router)
 app.use(CoreuiVue)
+app.use(vuetify)
 app.provide('icons', icons)
 app.component('CIcon', CIcon)
 
