a78e6b97b37299127795a52b5b4b467b63938273
Rundredoffi <contact.rundred@gmail.com>
2025-02-28 05:51:26 +0100

    ✨ Ajout du logo Track-IT dans la barre latérale de l'application

--- src/components/AppSidebar.vue
+++ src/components/AppSidebar.vue
@@ -19,7 +19,9 @@ const sidebar = useSidebarStore()
     <CSidebarHeader class="border-bottom">
       <RouterLink custom to="/" v-slot="{ href, navigate }">
         <CSidebarBrand v-bind="$attrs" as="a" :href="href" @click="navigate">
-          
+          <span class="d-flex justify-content-center">
+            <img src="/src/assets/brand/track-it-white.png" class="w-75" alt="Track-IT Logo" />
+          </span>
         </CSidebarBrand>
       </RouterLink>
       <CCloseButton class="d-lg-none" dark @click="sidebar.toggleVisible()" />
