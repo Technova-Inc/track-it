ed3c1f7ab1ff560443460962db5c34243df018a6
Rundredoffi <contact.rundred@gmail.com>
2025-02-28 05:51:22 +0100

    🔧 Mise à jour du pied de page pour refléter les nouveaux crédits et le framework utilisé

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,14 +1,11 @@
 <template>
   <CFooter class="px-4">
     <div>
-      <a href="https://coreui.io" target="_blank">CoreUI</a>
-      <span class="ms-1"
-        >&copy; {{ new Date().getFullYear() }} creativeLabs.</span
-      >
+      Marin & Nicolas &copy; {{ new Date().getFullYear() }}
     </div>
     <div class="ms-auto">
-      <span class="me-1" target="_blank">Powered by</span>
-      <a href="https://coreui.io/vue">CoreUI for Vue</a>
+      <span class="me-1" target="_blank">Framework : </span>
+      <a href="https://coreui.io/vue">CoreUI</a>
     </div>
   </CFooter>
 </template>
