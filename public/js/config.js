507aa12c856d6bc1b30f377f8abbb653bc06fd14
Rundredoffi <contact.rundred@gmail.com>
2025-03-14 09:42:04 +0100

    :wrench: Ajout configuration Axios

---
+++ public/js/config.js
@@ -0,0 +1,8 @@
+// Environnement applicatif
+window.VUE_APP_ENV = 'dev';
+// URL de l'API
+window.API_URL = 'http://localhost:8080';
+// Statut de maintenance
+window.MAINTENANCE = 'false';
+window.MAINTENANCE_TITLE = '';
+window.MAINTENANCE_MESSAGE = '';
