cf7bd4979acdb6c54f5922bb934f0bf50d3e91de
Rundredoffi <contact.rundred@gmail.com>
2025-03-21 08:48:42 +0100

    chore: Update API URL in config.js file

--- public/js/config.js
+++ public/js/config.js
@@ -1,7 +1,7 @@
 // Environnement applicatif
 window.VUE_APP_ENV = 'dev';
 // URL de l'API
-window.API_URL = 'http://localhost:8080';
+window.API_URL = 'http://10.29.126.31/track-it/index.php/';
 // Statut de maintenance
 window.MAINTENANCE = 'false';
 window.MAINTENANCE_TITLE = '';
