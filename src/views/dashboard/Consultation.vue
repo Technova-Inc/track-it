b55010d8535438daed40a5204724c21d14557bd5
Marin <marin.cadro@gmail.com>
2025-04-25 14:21:38 +0200

    :wrench: Fix Consultation

--- src/views/dashboard/Consultation.vue
+++ src/views/dashboard/Consultation.vue
@@ -10,9 +10,9 @@
             <CListGroup>
               <CListGroupItem> OS : {{ pcData.OSNAME }} </CListGroupItem>
               <CListGroupItem> OS Version : {{ pcData.OSVERSION }} </CListGroupItem>
-              <CListGroupItem> Architecture : {{ pcData.ARCH }} </CListGroupItem>
-              <CListGroupItem> Utilisateur windows : {{ pcData.WINOWNER }} </CListGroupItem>
-              <CListGroupItem> Licence windows : {{ pcData.WINPRODID }} </CListGroupItem>
+              <CListGroupItem> Architecture : {{ pcData.ARCHITECTURE }} </CListGroupItem>
+              <CListGroupItem> Utilisateur windows : {{ pcData.USER }} </CListGroupItem>
+              <CListGroupItem> Licence status : {{ pcData.licensestatus }} </CListGroupItem>
               <CListGroupItem> Clé windows : {{ pcData.WINPRODKEY }} </CListGroupItem>
             </CListGroup>
           </CCardBody>
@@ -22,7 +22,7 @@
           <CCardBody>
             <CCardTitle class="text-center">Hardware</CCardTitle>
             <CListGroup>
-              <CListGroupItem> Swap : {{ pcData.SWAP }} </CListGroupItem>
+              <CListGroupItem> CPU : {{ pcData.CPU }} </CListGroupItem>
               <CListGroupItem> RAM : {{ pcData.MEMORY }} octets </CListGroupItem>
               <CListGroupItem> UUID : {{ pcData.UUID }} </CListGroupItem>
             </CListGroup>
@@ -34,7 +34,7 @@
           <CCardBody>
             <CCardTitle class="text-center">Réseau</CCardTitle>
             <CListGroup>
-              <CListGroupItem> Domaine : {{ pcData.WORKGROUP }} </CListGroupItem>
+              <CListGroupItem> Domaine : {{ pcData.DOMAIN }} </CListGroupItem>
               <CListGroupItem> Adresse IP : {{ pcData.IPADDR }} </CListGroupItem>
             </CListGroup>
           </CCardBody>
@@ -44,7 +44,7 @@
           <CCardBody>
             <CCardTitle class="text-center">Agent</CCardTitle>
             <CListGroup>
-              <CListGroupItem> User agent : {{ pcData.USERAGENT }} </CListGroupItem>
+              <!-- <CListGroupItem> User agent : {{ pcData.USERAGENT }} </CListGroupItem> -->
               <CListGroupItem> Dernier inventaire : {{ pcData.LASTDATE }} </CListGroupItem>
               <CListGroupItem> Dernier contact : {{ pcData.LASTCOME }} </CListGroupItem>
             </CListGroup>
@@ -54,9 +54,9 @@
         <CCard>
           <CCardBody>
             <CCardTitle class="text-center">Notes</CCardTitle>
-            <textarea class="form-control" aria-label="Notes"></textarea>
+            <textarea class="form-control" aria-label="Notes" v-model="note"></textarea>
             <div class="d-grid gap-2 mt-3">
-              <button class="btn btn-primary" type="button" @click="saveNotes">Sauvegarder</button>
+              <button class="btn btn-primary text-white" type="button" @click="saveNotes">Sauvegarder</button>
             </div>
           </CCardBody>
         </CCard>
@@ -77,24 +77,29 @@ const props = defineProps({
 
 // Define reactive data properties
 const pcData = ref({
-  OSNAME: '',
   OSVERSION: '',
-  ARCH: '',
-  WINOWNER: '',
+  OSNAME: '',
+  ARCHITECTURE: '',
+  USER: '',
   WINPRODID: '',
   WINPRODKEY: '',
-  SWAP: '',
+  CPU: '',
   MEMORY: '',
   UUID: '',
-  WORKGROUP: '',
+  DOMAIN: '',
   IPADDR: '',
   USERAGENT: '',
   LASTDATE: '',
   LASTCOME: '',
+  licensestatus: '',
+  NOTE: '',
 })
 
 const route = useRoute()
 const pcId = route.params.id
+
+const note = ref('')
+
 
 // Fetch data from API
 const fetchPcData = async (id) => {
@@ -102,6 +107,8 @@ const fetchPcData = async (id) => {
     const response = await axios.get(`/Consultation?pc=${id}`)
     if (response.data?.pc && response.data.pc.length > 0) {
       pcData.value = response.data.pc[0]
+      note.value = pcData.value.NOTE
+
     } else {
       console.error("Aucune donnée trouvée pour l'ID spécifié")
     }
@@ -119,3 +126,18 @@ const saveNotes = () => {
   // Your save notes logic here
 }
 </script>
+<style>
+.dark-table {
+  background-color: #1d222b !important;
+  color: white !important;
+}
+
+.light-table {
+  background-color: #f3f4f7 !important;
+  color: black !important;
+}
+
+.v-data-table__th:hover {
+  color: #5856d6 !important;
+}
+</style>
